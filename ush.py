e59e8ec36d0c4fe3da854035f1ccea1fc14fc4c1
JohnLenino <konstantinbykov3122008@gmail.com>
2024-02-12 23:10:58 +0700

    второе задание

--- ush.py
+++ ush.py
@@ -1,18 +1,16 @@
+# app.py (Flask приложение)
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/8080')
 def index():
-    return "Миссия Колонизация Марса"
+    return "Добро пожаловать на страницу миссии на Марс!"
 
-@app.route('/index')
-def f():
-    title = 'da'
-    return render_template('base.html', title=title)
-
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('base.html', prof=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
