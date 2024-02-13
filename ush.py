2c843ff2be7e16f4ad52afe8b8c6dd961e62a9aa
JohnLenino <konstantinbykov3122008@gmail.com>
2024-02-13 14:23:27 +0700

    третье задание

--- ush.py
+++ ush.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 def index():
     return "Добро пожаловать на страницу миссии на Марс!"
 
-@app.route('/training/<prof>')
-def training(prof):
-    return render_template('base.html', prof=prof)
+@app.route('/<list_prof>/<list2>')
+def training(list_prof, list2):
+    print(list2, list_prof)
+    return render_template('base.html', list_prof=list_prof, list2=list2)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
