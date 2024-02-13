4c7cd53d2ad4f9ad7489bf91ac3eed418dfa40f0
JohnLenino <konstantinbykov3122008@gmail.com>
2024-02-13 14:30:01 +0700

    третье задание (норм версия)

--- ush.py
+++ ush.py
@@ -8,9 +8,21 @@ app = Flask(__name__)
 def index():
     return "Добро пожаловать на страницу миссии на Марс!"
 
-@app.route('/<list_prof>/<list2>')
-def training(list_prof, list2):
-    print(list2, list_prof)
+@app.route('/list_prof/<list2>')
+def training(list2):
+    print(list2)
+    list_prof = [
+        "Инженер-конструктор",
+        "Биолог",
+        "Астрофизик",
+        "Врач",
+        "Пилот-исследователь",
+        "Геолог",
+        "Компьютерный инженер",
+        "Психолог",
+        "Агроном",
+        "Электротехник"
+    ]
     return render_template('base.html', list_prof=list_prof, list2=list2)
 
 if __name__ == '__main__':
